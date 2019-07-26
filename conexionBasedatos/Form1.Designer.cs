c701e63c5d19f7ca1839f318ef084b5b42103854
Luis Gutierrez <alejandroluis44@gmail.com>
2019-07-26 12:08:22 -0500

    subiendo actualizacion

---
+++ conexionBasedatos/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace conexionBasedatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrimerapellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSegundoapellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.txtgenero = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(135, 130);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(160, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtPrimerapellido
+            // 
+            this.txtPrimerapellido.Enabled = false;
+            this.txtPrimerapellido.Location = new System.Drawing.Point(135, 177);
+            this.txtPrimerapellido.Name = "txtPrimerapellido";
+            this.txtPrimerapellido.Size = new System.Drawing.Size(160, 20);
+            this.txtPrimerapellido.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Primer apellido: ";
+            // 
+            // txtSegundoapellido
+            // 
+            this.txtSegundoapellido.Enabled = false;
+            this.txtSegundoapellido.Location = new System.Drawing.Point(135, 229);
+            this.txtSegundoapellido.Name = "txtSegundoapellido";
+            this.txtSegundoapellido.Size = new System.Drawing.Size(160, 20);
+            this.txtSegundoapellido.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Segundo apellido: ";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Enabled = false;
+            this.txtEdad.Location = new System.Drawing.Point(135, 280);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(160, 20);
+            this.txtEdad.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Genero:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(47, 453);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(82, 35);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(323, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(733, 224);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Enabled = false;
+            this.txtCelular.Location = new System.Drawing.Point(135, 381);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(160, 20);
+            this.txtCelular.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Celular:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(157, 453);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(82, 35);
+            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(263, 453);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(82, 35);
+            this.btnActualizar.TabIndex = 15;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Enabled = false;
+            this.txtDocumento.Location = new System.Drawing.Point(135, 85);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(160, 20);
+            this.txtDocumento.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Documento: ";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(370, 453);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(82, 35);
+            this.btnNuevo.TabIndex = 18;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // txtgenero
+            // 
+            this.txtgenero.Enabled = false;
+            this.txtgenero.Location = new System.Drawing.Point(135, 334);
+            this.txtgenero.Name = "txtgenero";
+            this.txtgenero.Size = new System.Drawing.Size(160, 20);
+            this.txtgenero.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 684);
+            this.Controls.Add(this.txtgenero);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSegundoapellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrimerapellido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrimerapellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSegundoapellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox txtgenero;
+    }
+}
+
---
+++ conexionBasedatos/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace conexionBasedatos
+{
+    public partial class Form1 : Form
+    {
+        String Base = "datasource=localhost; port=3306; username=Alex; password=3004353603.2;database=pruebaconexion;";
+            
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+
+        public void mostrar()
+        {
+
+            
+
+            String query = "";
+            
+            query = "SELECT Documento,Nombres, Primer_apellido, Segundo_apellido, Edad, Genero,Celular FROM Usuario";
+            
+
+            MySqlConnection conexion = new MySqlConnection(Base);
+
+            conexion.Open();
+
+            MySqlCommand comando = new MySqlCommand(query, conexion);
+            
+
+
+
+            MySqlDataAdapter dap = new MySqlDataAdapter(comando);
+
+            DataSet dast = new DataSet();
+
+            dap.Fill(dast);
+
+            dataGridView1.DataSource = dast.Tables[0];
+            conexion.Close();
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           mostrar();
+            txtDocumento.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            String query1 = "";
+            Console.Write("");
+
+
+            query1 = @"INSERT INTO Usuario values(?idUsuario,?Documento,?Nombres, ?Primer_apellido, ?Segundo_apellido, ?Edad, ?Genero, ?Celular)";
+            MySqlConnection conexion = new MySqlConnection(Base);
+
+            conexion.Open();
+            MySqlCommand coman = new MySqlCommand(query1, conexion);
+
+            coman.Parameters.AddWithValue("?idUsuario", null);
+            coman.Parameters.AddWithValue("?Documento", txtDocumento.Text.ToUpper());
+            coman.Parameters.AddWithValue("?Nombres", txtNombre.Text.ToUpper());
+            coman.Parameters.AddWithValue("?Primer_apellido", txtPrimerapellido.Text.ToUpper());
+            coman.Parameters.AddWithValue("?Segundo_apellido", txtSegundoapellido.Text.ToUpper());
+            coman.Parameters.AddWithValue("?Edad", Convert.ToInt64(txtEdad.Text));
+            coman.Parameters.AddWithValue("?Genero", txtgenero.Text.ToUpper());
+            coman.Parameters.AddWithValue("?Celular", txtCelular.Text);
+            coman.ExecuteNonQuery();
+            conexion.Close();
+            mostrar();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = " ";
+                    c.Enabled = true;
+                    txtDocumento.Focus();
+                }
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
